378b24aa51cadd392694c0bb5d001b519a040ef4
Marcel Keller <marcel.keller1993@gmail.com>
2020-03-03 19:54:34 +0100

    add spot report now possible

---
+++ src/app/add-spot-report-dialog/add-spot-report-dialog.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { SpotReport } from '../model/spotreport';
+import { SpotReportService } from '../services/spot-report.service';
+
+
+@Component({
+  selector: 'app-add-spot-report-dialog',
+  templateUrl: './add-spot-report-dialog.component.html',
+  styleUrls: ['./add-spot-report-dialog.component.scss']
+})
+export class AddSpotReportDialogComponent implements OnInit {
+
+  public report: SpotReport;
+  public isMapVisible: boolean;
+
+  constructor(private dialogRef: MatDialogRef<AddSpotReportDialogComponent>, private service: SpotReportService) { }
+
+  ngOnInit(): void {
+    this.report = new SpotReport();
+    this.report.spotLocation = { accuracy: 1, longitude: 0, latitude: 0 };
+  }
+
+  onMapClicked(event: google.maps.MouseEvent): void {
+    console.log(event);
+    let location = event.latLng;
+    this.report.spotLocation.latitude = location.lat();
+    this.report.spotLocation.longitude = location.lng();
+    this.isMapVisible = false;
+  }
+
+  save() {
+    
+    this.service.addSpotReport(this.report).subscribe(result => {
+      console.log('Create:' + JSON.stringify(result));
+
+      this.dialogRef.close(result);
+    });
+  }
+
+  dismiss() {
+    this.dialogRef.close();
+  }
+
+  showMap() {
+    this.isMapVisible = true;
+  }
+
+}
