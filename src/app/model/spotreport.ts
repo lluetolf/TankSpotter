889e33f89b3c9ceea2400f45988f1802a31d9e5b
Lukas <lukas.luetolf@gmail.com>
2020-03-03 11:21:45 +0100

    Create SpotReport model

---
+++ src/app/model/spotreport.ts
@@ -0,0 +1,14 @@
+import { StringifyOptions } from 'querystring'
+
+export class SpotReport {
+    id: number
+    created: Date
+    sensor: string
+    sender: string
+    status: string
+    recipient: string
+    observerLocation: Coordinates
+    spotLocation: Coordinates
+    spotTime: Date
+    tankType: string
+}
